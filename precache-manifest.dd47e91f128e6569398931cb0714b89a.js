34738a9047cd99799d3b9daac8b320837c7a5b4d
Chau Giang <giang.ntc@temando.com>
2019-06-24 20:39:36 +0700

    Updates

---
+++ precache-manifest.dd47e91f128e6569398931cb0714b89a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c103fe10cf9eaed9062b059fae81f470",
+    "url": "/ipa-eng/index.html"
+  },
+  {
+    "revision": "bbd3728a2f2b681bb4c0",
+    "url": "/ipa-eng/static/css/main.85be3c53.chunk.css"
+  },
+  {
+    "revision": "a004ac3ee9cc7f846dd2",
+    "url": "/ipa-eng/static/js/2.c31d4468.chunk.js"
+  },
+  {
+    "revision": "bbd3728a2f2b681bb4c0",
+    "url": "/ipa-eng/static/js/main.54081fcc.chunk.js"
+  },
+  {
+    "revision": "e16e181a2c1e4ae3a3e9",
+    "url": "/ipa-eng/static/js/runtime~main.67f50c22.js"
+  }
+]);
